d5aa7825261bb93b433a87e097c835ae0bd370b0
Manuel Bilbao <bilbaomanuel98@gmail.com>
2022-04-17 14:40:18 -0300

    Quick Heroku fix

--- app/database.py
+++ app/database.py
@@ -7,7 +7,11 @@ from sqlalchemy.orm import sessionmaker
 SQLALCHEMY_DATABASE_URL = os.environ.get("DATABASE_URL",
                                          "sqlite:///./db.sqlite3")
 # Heroku deprecated url fix
-SQLALCHEMY_DATABASE_URL.replace("postgres://", "postgresql://", 1)
+SQLALCHEMY_DATABASE_URL = SQLALCHEMY_DATABASE_URL.replace(
+    "postgres://",
+    "postgresql://",
+    1
+)
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit = False,
