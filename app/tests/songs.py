8ccdd84c6455b8998c88be7c5470885c80618a47
Manuel Bilbao <bilbaomanuel98@gmail.com>
2022-04-25 11:12:13 -0300

    update tests

--- app/tests/songs.py
+++ app/tests/songs.py
@@ -1,7 +1,8 @@
-from .base import client
+from fastapi.testclient import TestClient
+from sqlalchemy.orm import Session
 
 
-def test_get_songs():
+def test_get_songs(client: TestClient, db: Session) -> None:
     response = client.get("/songs/")
     assert response.status_code == 200
     assert type(response.json()) == list
