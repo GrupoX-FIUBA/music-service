18142bacace8b2b6d9cc56aed669d3b1debf3b75
Manuel Bilbao <bilbaomanuel98@gmail.com>
2022-05-16 17:05:15 -0300

    Added API Key env to deploy

--- app/core/settings.py
+++ app/core/settings.py
@@ -4,4 +4,4 @@ DATABASE_URL = os.environ["DATABASE_URL"].replace("postgres://",
                                                   "postgresql://", 1)
 
 API_KEY_NAME = "X-API-Key"
-API_KEY = os.environ.get("X-API-Key", "prueba")
+API_KEY = os.environ.get("X_API_KEY", "prueba")
