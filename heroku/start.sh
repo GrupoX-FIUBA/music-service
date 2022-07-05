20a6086b936dadfd0589ad6afc3339881edd8982
Ezequiel Zbogar <44110680+ezezbogar@users.noreply.github.com>
2022-07-04 22:15:17 -0300

    Datadog config (#10)

---
+++ heroku/start.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+alembic upgrade head
+
+datadog-agent run > /dev/null &
+/opt/datadog-agent/embedded/bin/trace-agent --config=/etc/datadog-agent/datadog.yaml > /dev/null &
+/opt/datadog-agent/embedded/bin/process-agent --config=/etc/datadog-agent/datadog.yaml > /dev/null &
+
+DD_SERVICE="music-service" \
+DD_ENV="prod" \
+DD_LOGS_INJECTION=true \
+ddtrace-run uvicorn app.main:app --host 0.0.0.0 --port $PORT
