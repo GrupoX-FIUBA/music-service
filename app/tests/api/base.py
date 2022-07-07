896fed8f5864fa0d55437326c8f877bd7ceec853
Manuel Bilbao <bilbaomanuel98@gmail.com>
2022-07-07 16:57:56 -0300

    Test database

--- app/tests/api/base.py
+++ app/tests/api/base.py
@@ -1,4 +1,28 @@
-from app.core.settings import API_KEY_NAME, API_KEY
+from app.core.settings import API_KEY_NAME, API_KEY, DATABASE_URL
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+from app.main import app
+from app.db.base_class import Base
+from app.endpoints.base import get_db
+
+
+engine = create_engine(DATABASE_URL)
+TestingSessionLocal = sessionmaker(autocommit = False, autoflush = False,
+                                   bind = engine)
+
+Base.metadata.create_all(bind = engine)
+
+
+def override_get_db():
+    try:
+        db = TestingSessionLocal()
+        yield db
+    finally:
+        db.close()
+
+
+app.dependency_overrides[get_db] = override_get_db
 
 
 def get_valid_api_key():
